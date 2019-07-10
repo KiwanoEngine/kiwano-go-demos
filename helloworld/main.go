086c8b3c32c83ba29e2d4e4a0a0d2d0b33cba833
Nomango <nomango@qq.com>
2019-07-10 17:21:18 +0800

    update

--- helloworld/main.go
+++ helloworld/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"math"
 	"time"
 
 	"github.com/go-gl/gl/v3.3-core/gl"
@@ -14,39 +15,51 @@ import (
 var vertexShaderSource = `
 #version 330 core
 layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
+
+out vec3 ourColor;
 
 void main()
 {
-    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+	ourColor = aColor;
 }
 `
 
 var fragmentShaderSource = `
 #version 330 core
+in vec3 ourColor;
 out vec4 FragColor;
 
 void main()
 {
-    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    FragColor = vec4(ourColor, 1.0);
 }
 `
 
 var vertices = []float32{
-	-0.5, -0.5, 0.0, // left
-	0.5, -0.5, 0.0, // right
-	0.0, 0.5, 0.0, // top
+	// 位置          // 颜色
+	0.5, -0.5, 0.0, 1.0, 0.0, 0.0, // 右下
+	-0.5, -0.5, 0.0, 0.0, 1.0, 0.0, // 左下
+	0.0, 0.5, 0.0, 0.0, 0.0, 1.0, // 顶部
+}
+
+var indices = []uint32{ // 注意索引从0开始!
+	0, 1, 3, // 第一个三角形
+	1, 2, 3, // 第二个三角形
 }
 
 type MainScene struct {
-	VAO           uint32
-	VBO           uint32
-	shaderProgram uint32
+	VAO    uint32
+	VBO    uint32
+	EBO    uint32
+	shader *render.Shader
 }
 
 func (s *MainScene) OnEnter() {
 	// Create shader program
 	var err error
-	s.shaderProgram, err = render.CreateShaderProgram(vertexShaderSource, fragmentShaderSource)
+	s.shader, err = render.CreateShader(vertexShaderSource, fragmentShaderSource)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -60,8 +73,15 @@ func (s *MainScene) OnEnter() {
 	gl.BindBuffer(gl.ARRAY_BUFFER, s.VBO)
 	gl.BufferData(gl.ARRAY_BUFFER, 4*len(vertices), gl.Ptr(vertices), gl.STATIC_DRAW)
 
+	gl.GenBuffers(1, &s.EBO)
+	gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, s.EBO)
+	gl.BufferData(gl.ELEMENT_ARRAY_BUFFER, 4*len(indices), gl.Ptr(indices), gl.STATIC_DRAW)
+
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 6*4, gl.PtrOffset(0))
 	gl.EnableVertexAttribArray(0)
-	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 0, nil)
+
+	gl.VertexAttribPointer(1, 3, gl.FLOAT, false, 6*4, gl.PtrOffset(3*4))
+	gl.EnableVertexAttribArray(1)
 }
 
 func (s *MainScene) OnUpdate(dt time.Duration) {
@@ -69,13 +89,18 @@ func (s *MainScene) OnUpdate(dt time.Duration) {
 		kiwano.Window.GLFWWindow.SetShouldClose(true)
 	}
 
-	gl.UseProgram(s.shaderProgram)
-	gl.BindVertexArray(s.VAO)
-	gl.DrawArrays(gl.TRIANGLES, 0, 3)
+	timeValue := glfw.GetTime()
+	greenValue := (math.Sin(timeValue) / 2.0) + 0.5
+	vertexColorLocation := gl.GetUniformLocation(s.shader.ID, gl.Str("ourColor\x00"))
+
+	s.shader.Use()
+	gl.Uniform4f(vertexColorLocation, 0.0, float32(greenValue), 0.0, 1.0)
+
+	//gl.DrawArrays(gl.TRIANGLES, 0, 3)
+	gl.DrawElements(gl.TRIANGLES, 6, gl.UNSIGNED_INT, nil)
 }
 
 func (s *MainScene) OnExit() {
-	gl.DeleteProgram(s.shaderProgram)
 	gl.DeleteVertexArrays(1, &s.VAO)
 	gl.DeleteBuffers(1, &s.VBO)
 }
@@ -83,14 +108,14 @@ func (s *MainScene) OnExit() {
 func main() {
 	// Init kiwano engine
 	if err := kiwano.Init(&core.Option{
-		Width:           640,
-		Height:          480,
-		Title:           "LearnOpenGL",
-		BackgroundColor: core.ColorRGB(0.2, 0.3, 0.3),
-		NoTitleBar:      false,
-		Resizable:       true,
-		Fullscreen:      false,
-		Vsync:           true,
+		Width:      640,
+		Height:     480,
+		Title:      "LearnOpenGL",
+		ClearColor: core.ColorRGB(0.2, 0.3, 0.3),
+		NoTitleBar: false,
+		Resizable:  true,
+		Fullscreen: false,
+		Vsync:      true,
 	}); err != nil {
 		log.Fatalln(err)
 	}
